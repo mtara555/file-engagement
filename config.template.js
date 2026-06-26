82e01115cf45c748ac50e89dbb60432f64fd9cb7
MOHAMED TARABET <80125771+mtara555@users.noreply.github.com>
2026-06-26 23:14:47 +0100

    Update version number to 2.0.0

--- config.template.js
+++ config.template.js
@@ -2,5 +2,5 @@ const CONFIG = {
     SUPABASE_URL  : '__SUPABASE_URL__',
     SUPABASE_ANON : '__SUPABASE_ANON__',
     APP_NAME      : 'File Engagement — Marjane Holding',
-    VERSION       : '1.0.0'
+    VERSION       : '2.0.0'
 };
